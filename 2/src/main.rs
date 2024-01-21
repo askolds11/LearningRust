4945bddeaa8b552ee1d1b669cea11a061eb5bb1f
askolds <askolds11@gmail.com>
2024-01-21 02:54:35 +0200

    2 - Guessing game

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -1,17 +1,46 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, cmp::Ordering};
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    print!("Please input your guess: ");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is: {secret_number}");
 
-    io::stdout().flush().expect("Flush failed!");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed {guess}");
+    loop {
+        print!("Please input your guess: ");
+        io::stdout().flush().expect("Flush failed!");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+    
+        // the read_line contains the new line character from input
+        // and as such causes a blank line in the ouput
+        // guess = guess.trim().to_string();
+        // println!("You guessed {guess}");
+        
+    
+        let guess: u8 = match guess
+            .trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Please enter a valid number!");
+                    continue;
+                }
+            };
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println! ("You win!");
+                break;
+            }
+        }               
+    }
 }
